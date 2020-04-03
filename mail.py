f68ee0ac6ba97039b6da43f432f17cb0645f5aca
purushotham966 <42888562+purushotham966@users.noreply.github.com>
2020-04-03 18:50:38 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,14 +1,15 @@
+#**if you access google account on other applications ,you should turn less secure app "ON" in your accounts
+#just search "less secure apps" on google to get direct link.
+
+
 import smtplib
 try:
-    
-                
-                
                 
     mail=smtplib.SMTP('smtp.gmail.com:587')
     mail.ehlo()
     mail.starttls()
-    mail.login("tekuripurushotham@gmail.com","Sreenu@966")
-    mail.sendmail("tekuripurushotham@gmail.com","tekuripurushotham@gmail.com","unknown person detected")
+    mail.login("write receiver mail_id","write_password")
+    mail.sendmail("receiver mail_id","sender mail_id","unknown person detected")
     mail.close()
     print("Mail sent")
     
